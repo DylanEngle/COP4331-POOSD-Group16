2c813a836576c4ed68051d2204becad7dd914448
fab0777 <122804819+fab0777@users.noreply.github.com>
2023-01-23 17:59:08 -0500

    Add files via upload

---
+++ API/AddUser.php
@@ -0,0 +1,41 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$login = $inData["login"];
+	$password = $inData["password"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (login,password,firstName,lastName) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssss", $login, $password, $firstName, $lastName);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
